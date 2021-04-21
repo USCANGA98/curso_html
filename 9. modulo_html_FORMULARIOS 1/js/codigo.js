7b2bb8071bae3e0b061f20123b4b9032dbbc908c
USCANGA98 <macarioluisangel006@gmail.com>
2021-04-20 21:03:34 -0500

    Se agregaron las carpetas 9, 10, 11 y 12

---
+++ 9. modulo_html_FORMULARIOS 1/js/codigo.js
@@ -0,0 +1,5 @@
+
+let num = prompt("Ingresa el primero");
+let num2 = prompt("Ingresa el segundo numero");
+console.log(`El resultado de la suma de ${num} + ${num2} = ${parseInt(num)+parseInt(num2)}`);
+alert("Hola, estas en un documento de Javascript");
